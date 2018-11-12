0cfc540682b24220dc2cee980a8e2f0d76ab1132
Marcelolac <44480740+Marcelolac@users.noreply.github.com>
2018-11-12 11:43:57 -0200

    Update VoterRepository.java

--- src/main/java/br/edu/ulbra/election/voter/repository/VoterRepository.java
+++ src/main/java/br/edu/ulbra/election/voter/repository/VoterRepository.java
@@ -4,8 +4,6 @@ import br.edu.ulbra.election.voter.model.Voter;
 import org.springframework.data.repository.CrudRepository;
 
 public interface VoterRepository extends CrudRepository<Voter, Long> {
-	@Query("select e from #{#entityName} e where e.year = ?1")
-	List<Election> findByYear(Integer year);
-    Voter findByEmail(String email);
+	Voter findByEmail(String email);
 	Voter findByName(String votername);
 }
